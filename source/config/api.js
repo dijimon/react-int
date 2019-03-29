cb9e0c41021d36897e2aae5017d1c5cb21dd02dd
Dmitry Zakharchenko <zah_dm@hotmail.com>
2019-03-29 16:19:05 +0200

    fix: initial commit with group_id and token

--- source/config/api.js
+++ source/config/api.js
@@ -1,8 +1,8 @@
 // Core
 import { getFullApiUrl } from 'instruments';
 
-const GROUP_ID = '';
-const TOKEN = '';
+const GROUP_ID = '97logdaump6s';
+const TOKEN = 'wpnrps51ln';
 const url = 'https://lab.lectrum.io/react/api';
 const api = getFullApiUrl(url, GROUP_ID);
 
